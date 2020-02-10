f348ca9187d44cceb3eac589d6cd82cad54d9a4d
Marco F <m.faverof@gmail.com>
2020-02-10 16:54:20 +0100

    remove del os

--- wildfire-milter.py
+++ wildfire-milter.py
@@ -331,7 +331,6 @@ class WildfireMilter(Milter.Base):
         global r
         global rsub
         global wfp
-        import os
         all_files_to_inspect = []
         tmpdir = None
         tmpdirs = []
@@ -401,7 +400,6 @@ class WildfireMilter(Milter.Base):
                                                                     self.id, self.queueid))
             if OPTIMIZE_APICALL:
                 wildlib.cleanup(all_files_to_inspect, tmpdirs, logadd)
-        del os
         return verdicts
 
     def milter_result(self, dict_verdicts):
